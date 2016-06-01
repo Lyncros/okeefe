2c94039970fca181c18bdf170fbfccc2318512ad
Lucas Michailian <lucasmichailian@gmail.com>
2016-06-01 09:40:36 -0300

    models for api

---
+++ app/Http/api_routes.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where all API routes are defined.
+|
+*/
+
+Route::resource('caracteristicas', 'CaracteristicaAPIController');
+
+Route::resource('domicilios', 'DomicilioAPIController');
+
+Route::resource('localidades', 'LocalidadAPIController');
+
+Route::resource('pais', 'PaisAPIController');
