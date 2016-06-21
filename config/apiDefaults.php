b373c065d261696e3bb02eee08a5d16ee5fc596c
Lucas Michailian <lucasmichailian@gmail.com>
2016-06-21 11:58:44 -0300

    default docs changes

--- config/apiDefaults.php
+++ config/apiDefaults.php
@@ -1,5 +1,11 @@
 <?php
 
+
+/**
+ * Valores por defecto de las consultas, si se cambia el
+ * nombre del key de cualquier valor se debe cambiar el mismo
+ * en la query de \App\Repositories\PropiedadRepository
+ */
 return [
     'operacion' => 12,
 
@@ -13,12 +19,15 @@ return [
 
     'supMax' => 100000000,
 
+    // Valores con opciones
     'moneda' => [
         'options' => ['U$S', '$']
     ],
 
     'banos' => 1,
 
+    // Los valores que tienen true se reflejan como < 100 en el query y la consulta especifica como,
+    // nombre==valor
     'amb' => true,
 
     'coch' => true,
