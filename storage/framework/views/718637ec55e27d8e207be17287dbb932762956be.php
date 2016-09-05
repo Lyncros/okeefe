725b81cb92c2f67c8753ae18a16c7698d15f136d
Lucas Michailian <lucasmichailian@gmail.com>
2016-09-05 14:59:22 -0300

    fix folders

--- storage/framework/views/718637ec55e27d8e207be17287dbb932762956be.php
+++
@@ -1,3 +0,0 @@
-<?php echo $__env->make("inc.header_rural", array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-<?php require app_path().'/../public/templates/base/rural.html';?>
-<?php echo $__env->make("inc.footer_rural", array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
