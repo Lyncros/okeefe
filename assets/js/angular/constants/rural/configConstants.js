de59800ad2004b78a4266027c507b606adbb2ac8
Sebastián Navarrete <Sebastin Navarrete>
2016-09-23 14:28:03 -0500

    Cambio en archivo de constantes

--- assets/js/angular/constants/rural/configConstants.js
+++ assets/js/angular/constants/rural/configConstants.js
@@ -6,7 +6,7 @@
         .constant('API_SEARCH', 'http://search.okeefe.com.ar/api/v1/')
         .constant('API_JOB_APPLICATION', 'http://search.okeefe.com.ar/api/v1/jobApplication')
         //.constant('API_JOB_APPLICATION', 'http://localhost:8080/Lyncros/API-busqueda/public/jobApplication')
-        //.constant('SITE_URL', 'http://sitio.okeefe.com.ar/rural/#/')
-        .constant('SITE_URL', 'http://localhost:8080/Lyncros/Okeefe/okeefe/rural/#/')
+        .constant('SITE_URL', 'http://sitio.okeefe.com.ar/rural/#/')
+        //.constant('SITE_URL', 'http://localhost:8080/Lyncros/Okeefe/okeefe/rural/#/')
         .constant('API_OKEEFE', 'http://crm.okeefe.com.ar/api/v1/external-source/contacts');
 })();
