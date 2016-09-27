f332b55d52303e54824e57b2216b5be474114e90
Sebastián Navarrete <Sebastin Navarrete>
2016-09-27 10:37:32 -0500

    Agregado el formulario de venta hacienda a servicios rural

--- assets/js/angular/controllers/rural/servicesController.js
+++ assets/js/angular/controllers/rural/servicesController.js
@@ -6,6 +6,34 @@
                 dato: '',
                 error: false
             };
+            $scope.ventaHacForm = {
+                secret: 'sitiOkeefe',
+                dato: '',
+                error: false
+            };
+            $scope.ventaHacRuralForm = function ($event) {
+                $event.preventDefault();
+                if (!$scope.ventaHacForm.nombre || !$scope.ventaHacForm.apellido
+                    || !$scope.ventaHacForm.email || !$scope.ventaHacForm.telefono || !$scope.ventaHacForm.deseo) {
+                    $scope.ventaHacForm.error = true;
+                    return false;
+                }
+                $scope.ventaHacForm.comentarios = "<h3>Deseo " + $scope.ventaHacForm.deseo +"</h3>";
+                $scope.ventaHacForm.comentarios += ($scope.ventaHacForm.numero_animales) ? "<h4><strong>N°: </strong>" + $scope.ventaHacForm.numero_animales + "</h4>" : "";
+                $scope.ventaHacForm.comentarios += ($scope.ventaHacForm.categoria) ? "<h4><strong>Categoría: </strong>" + $scope.ventaHacForm.categoria + "</h4>" : "";
+                $scope.ventaHacForm.comentarios += ($scope.ventaHacForm.deseo == 'Vender' && $scope.ventaHacForm.valor) ? "<h4><strong>Valor: </strong>" + $scope.ventaHacForm.valor + "</h4>" : "";
+                okeefeApiService.API.send($scope.ventaHacForm).then(function (response) {
+                    entitiesService.showAlert($scope, 'Mensaje enviado. Estaremos en contacto en breve.', 'success', 3000);
+                    $scope.ventaHacForm = {
+                        secret: 'sitiOkeefe',
+                        dato: '',
+                        error: false
+                    };
+                }, function errorCallback(response) {
+                    entitiesService.showAlert($scope, 'Error al enviar el mensaje. Intenta de nuevo mas tarde.', 'danger', 3000);
+                    console.log("error :", response);
+                });
+            };
             $scope.alert = null;
             $scope.adminRuralForm = function ($event) {
                 $event.preventDefault();
