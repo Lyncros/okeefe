207179d8d56da74194e465e596d493ac20548c98
Sebastián Navarrete <Sebastin Navarrete>
2016-11-04 13:02:42 -0500

    Solución de bugs

--- assets/js/angular/controllers/rural/propertiesController.js
+++ assets/js/angular/controllers/rural/propertiesController.js
@@ -228,7 +228,7 @@
                     $scope.getParam();
                     searchApiService.searchApi.read($routeParams.tipo, $routeParams.operacion, $routeParams.ubicacion, $scope.param)
                         .then(function (response) {
-                            console.log("res", response.data.data);
+                            //console.log("res", response.data.data);
                             //$scope.properties = response.data.data[0].properties;
                             $scope.searchData = response.data.data[0];
                             //$scope.loadingProperties = false;
